fedb2450f5a5b915a43d06e74124c71db26dd550
StephKanji <kanjistephanie4@gmail.com>
2025-03-19 07:50:01 +0300

    removed price

--- src/worker.js
+++ src/worker.js
@@ -388,7 +388,7 @@ var worker_default = {
 async function saveFormData(d1, formData) {
   const { title, image, portion, nationality, price } = formData;
   await d1.prepare(
-    "INSERT INTO food_items (title, image, portion, nationality, price) VALUES (?1, ?2, ?3, ?4, ?5)"
+    "INSERT INTO food_items (title, image, portion, nationality) VALUES (?1, ?2, ?3, ?4)"
   ).bind(title, image, portion, nationality, price).run();
 }
 __name(saveFormData, "saveFormData");
